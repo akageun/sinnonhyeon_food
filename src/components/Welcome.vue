e154df8cf20377ac99556062855ecd74b063667b
akageun <akageun@gmail.com>
2018-12-03 18:54:38 +0900

    modify Welcome Vue File

---
+++ src/components/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Welcome',
+    data() {
+      return {
+        msg: '신논현 맛집 리스트'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
